251aba373bacf57f473f60f650e1afe5d68d1a97
Ernest Micklei <ernest.micklei@gmail.com>
2021-10-31 15:57:56 +0100

    use intransactiondo

--- todo/server/service.go
+++ todo/server/service.go
@@ -39,57 +39,41 @@ func (s *TodoImpl) CreateTodo(ctx context.Context, req *todo.CreateTodoRequest) 
 		return nil, fmt.Errorf("user %s not valid", "lisa")
 	}
 
-	// write to database and return new id
-
-	// begin
-	bResp, err := s.dbClient.Begin(ctx, new(apidb.BeginRequest))
-	if err != nil {
-		return nil, apilog.ErrorWithLog(ctx, err, "failed to begin db transaction", "db", "begin")
-	}
-
-	// mutate
-	mResp, err := s.dbClient.Mutate(ctx, &apidb.MutationRequest{
-		TransactionId: bResp.TransactionId,
-		Sql:           fmt.Sprintf("INSERT INTO public.tasks (title) VALUES ('hello microgate %v') RETURNING task_id", time.Now())})
-	if err != nil {
-		apilog.Debugw(ctx, "Rollback")
-		_, _ = s.dbClient.Rollback(ctx, &apidb.RollbackRequest{TransactionId: bResp.TransactionId})
-		return nil, apilog.ErrorWithLog(ctx, err, "failed to execute mutation", "db", "mutate")
-	}
-
-	// TEMP hack
-	apilog.Debugw(ctx, "insert result", "rows", mResp.Rows)
 	var id string
-	if mResp.Error == "" {
-		if len(mResp.Rows) > 0 {
-			if len(mResp.Rows[0].Columns) > 0 {
-				if mResp.Rows[0].Columns[0].GetValueIsValid() {
-					id = fmt.Sprintf("%d", mResp.Rows[0].Columns[0].GetInt32Value())
-					apilog.Debugw(ctx, "insert result", "id", id)
+	apidb.InTransactionDo(ctx, s.dbClient, func(transaction_id string) error {
+		mResp, err := s.dbClient.Mutate(ctx, &apidb.MutationRequest{
+			TransactionId: transaction_id,
+			Sql:           fmt.Sprintf("INSERT INTO public.tasks (title) VALUES ('hello microgate %v') RETURNING task_id", time.Now())})
+
+		// TEMP hack
+		apilog.Debugw(ctx, "insert result", "rows", mResp.Rows)
+		if mResp.Error == "" {
+			if len(mResp.Rows) > 0 {
+				if len(mResp.Rows[0].Columns) > 0 {
+					if mResp.Rows[0].Columns[0].GetValueIsValid() {
+						id = fmt.Sprintf("%d", mResp.Rows[0].Columns[0].GetInt32Value())
+						apilog.Debugw(ctx, "insert result", "id", id)
+					} else {
+						apilog.Debugw(ctx, "value is not valid")
+					}
 				} else {
-					apilog.Debugw(ctx, "value is not valid")
+					apilog.Debugw(ctx, "empty columns")
 				}
 			} else {
-				apilog.Debugw(ctx, "empty columns")
+				apilog.Debugw(ctx, "empty rows")
 			}
 		} else {
-			apilog.Debugw(ctx, "empty rows")
+			apilog.Debugw(ctx, "db error", "err", mResp.Error)
 		}
-	} else {
-		apilog.Debugw(ctx, "db error", "err", mResp.Error)
-	}
 
-	// commit
-	_, err = s.dbClient.Commit(ctx, &apidb.CommitRequest{TransactionId: bResp.TransactionId})
-	if err != nil {
-		return nil, apilog.ErrorWithLog(ctx, err, "failed to commit db transaction", "db", "commit")
-	}
-
-	// publish
-	_, err = s.queueClient.Publish(ctx, &apiqueue.PublishRequest{Topic: "todo", Message: []byte("Hello microgate")})
-	if err != nil {
-		return nil, apilog.ErrorWithLog(ctx, err, "failed to publish todo created")
-	}
+		// publish
+		_, err = s.queueClient.Publish(ctx, &apiqueue.PublishRequest{Topic: "todo", Message: []byte("Hello microgate")})
+		if err != nil {
+			return apilog.ErrorWithLog(ctx, err, "failed to publish todo created")
+		}
+		// all ok
+		return nil
+	})
 
 	return &todo.CreateTodoResponse{
 		Id: id,
