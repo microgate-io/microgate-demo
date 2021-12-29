7add76d06ccdf4fe07789a947e4de9a6d120d0ca
Ernest Micklei <ernest.micklei@gmail.com>
2021-12-29 14:38:05 +0100

    rm db

--- todo/server/service.go
+++ todo/server/service.go
@@ -39,41 +39,14 @@ func (s *TodoImpl) CreateTodo(ctx context.Context, req *todo.CreateTodoRequest) 
 		return nil, fmt.Errorf("user %s not valid", "lisa")
 	}
 
-	var id string
-	apidb.InTransactionDo(ctx, s.dbClient, func(transaction_id string) error {
-		mResp, err := s.dbClient.Mutate(ctx, &apidb.MutationRequest{
-			TransactionId: transaction_id,
-			Sql:           fmt.Sprintf("INSERT INTO public.tasks (title) VALUES ('hello microgate %v') RETURNING task_id", time.Now())})
+	// store a ToDo in your database
+	id := fmt.Sprintf("todo-%d", time.Now().UnixMilli())
 
-		// TEMP hack
-		apilog.Debugw(ctx, "insert result", "rows", mResp.Rows)
-		if mResp.Error == "" {
-			if len(mResp.Rows) > 0 {
-				if len(mResp.Rows[0].Columns) > 0 {
-					if mResp.Rows[0].Columns[0].GetValueIsValid() {
-						id = fmt.Sprintf("%d", mResp.Rows[0].Columns[0].GetInt32Value())
-						apilog.Debugw(ctx, "insert result", "id", id)
-					} else {
-						apilog.Debugw(ctx, "value is not valid")
-					}
-				} else {
-					apilog.Debugw(ctx, "empty columns")
-				}
-			} else {
-				apilog.Debugw(ctx, "empty rows")
-			}
-		} else {
-			apilog.Debugw(ctx, "db error", "err", mResp.Error)
-		}
-
-		// publish
-		_, err = s.queueClient.Publish(ctx, &apiqueue.PublishRequest{Topic: "todo", Message: []byte("Hello microgate")})
-		if err != nil {
-			return apilog.ErrorWithLog(ctx, err, "failed to publish todo created")
-		}
-		// all ok
-		return nil
-	})
+	// publish
+	_, err = s.queueClient.Publish(ctx, &apiqueue.PublishRequest{Topic: "todo", Message: []byte("Hello microgate")})
+	if err != nil {
+		return apilog.ErrorWithLog(ctx, err, "failed to publish todo created")
+	}
 
 	return &todo.CreateTodoResponse{
 		Id: id,
