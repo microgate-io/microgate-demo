9e6f65655593d02b105e97506cb17f9b218f5978
Ernest Micklei <ernest.micklei@gmail.com>
2022-01-06 16:55:10 +0100

    add header

--- todo/client/http/main.go
+++ todo/client/http/main.go
@@ -8,15 +8,16 @@ import (
 )
 
 func main() {
-	body := strings.NewReader(`
-{
-	"title": "hello microgate"
-}	
-	`)
+	body := strings.NewReader(`{"title":"hello microgate"}`)
 	resp, err := http.Post("http://localhost:8080/todo/v1/todo-service/create-todo", "application/json", body)
 	if err != nil {
 		log.Fatalln(err)
 	}
-	data, _ := ioutil.ReadAll(resp.Body)
+	log.Println(resp.Status)
+	defer resp.Body.Close()
+	data, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
 	log.Println(string(data))
 }
